723dcdfb2c57da019213ec8cc8d6d4b81b35cfb1
huazai5m <644827059@qq.com>
2020-05-11 13:24:53 +0800

    +++

--- config/dev.config.ts
+++ config/dev.config.ts
@@ -3,4 +3,17 @@ import { readFileSync } from 'fs';
 export default () => ({
   appId: '2016123456789012',
   privateKey: readFileSync('./hs_alipk.txt', 'ascii'),
+  database: {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    synchronize: true,
+    entities: ['src/**/*.entity{.ts,.js}'],
+    autoLoadEntities: true,
+    logging: true,
+    dropSchema: true,
+  },
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import devConfig from '../config/dev.config';
 import prodConfig from '../config/production.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,29 +9,31 @@ import { env } from 'process';
 const configPrams = { isGlobal: true, ignoreEnvFile: true, load: [] };
 
 if (env.NODE_ENV === 'development') {
+  console.log(env.NODE_ENV);
   configPrams.load = [devConfig];
 }
 
 if (env.NODE_ENV === 'production') {
+  console.log(env.NODE_ENV);
   configPrams.load = [prodConfig];
 }
 
 @Module({
   imports: [
     ConfigModule.forRoot(configPrams),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      synchronize: true,
-      entities: ['src/**/*.entity{.ts,.js}'],
-      autoLoadEntities: true,
-      logging: true,
-
-      dropSchema: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        type: config.get('database.type'),
+        host: config.get('database.host'),
+        port: config.get('database.port'),
+        username: config.get('database.username'),
+        password: config.get('database.password'),
+        database: config.get('database.database'),
+        entities: config.get('database.entities'),
+        synchronize: config.get('database.synchronize'),
+        logging: config.get('database.logging'),
+      }),
+      inject: [ConfigService],
     }),
     CatsModule,
   ],
